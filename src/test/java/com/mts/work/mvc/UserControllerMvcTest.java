fe073885dc1b9891801b2a8cac982d44d55a7b39
Alisa Galitsyna <galitsyna.ad@phystech.edu>
2025-02-23 20:26:33 +0300

    lesson 03 jwt, aop, actuator

--- src/test/java/com/mts/work/mvc/UserControllerMvcTest.java
+++ src/test/java/com/mts/work/mvc/UserControllerMvcTest.java
@@ -5,6 +5,7 @@ import com.mts.work.entity.User;
 import com.mts.work.entity.UserId;
 import com.mts.work.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class UserControllerMvcTest {
     @Autowired
     private MockMvc mockMvc;
